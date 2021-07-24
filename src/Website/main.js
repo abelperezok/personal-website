e04bef32f0a4dc7fef7f693f43f8b337fdccebc5
Abel Perez Martinez <abelperezok@gmail.com>
2021-07-24 15:49:24 +0100

    Updated message structure to match backend contract

--- src/Website/main.js
+++ src/Website/main.js
@@ -2,9 +2,9 @@ $("#contactForm").submit(function (event) {
     event.preventDefault();
     var $form = $(this);
     var msg = {
-        name: $("input[name=name]", $form).val(),
-        email: $("input[name=email]", $form).val(),
-        message: $("textarea[name=message]", $form).val()
+        Name: $("input[name=name]", $form).val(),
+        Email: $("input[name=email]", $form).val(),
+        Message: $("textarea[name=message]", $form).val()
     };
 
     $("#send-loading").removeClass("d-none");
